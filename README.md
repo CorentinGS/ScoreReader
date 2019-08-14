9482d6c2cd285aea88f21956f46d7a7e0565fefe
Yume <43623834+yumenetwork@users.noreply.github.com>
2019-08-14 08:46:08 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SheetReader
+
+
+## About
+
+SheetReader is an educational project in Python.
+
+The objective is to be able to play a music score with python and convert it into a MIDI file.
+
+This can also be added to LED bars on a piano to allow people who do not know musical
+theory or the visually impaired to play the piano.
+
+
+## Requirement
+
+- Python3.7
+- pip or conda
+
+## Status
+
+ **In Progress**
+ 
+## Author
+
+- [Yume](https://github.com/yumenetwork)
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+from pdf2image import convert_from_path
+
+
+def to_png(path, name):
+	images = convert_from_path(f"{path}/{name}.pdf")
+	images[0].save("temp/{}.png".format(name))
+
+
 def main():
-	print("Hello World")
+	path = "images"
+	name = "SheetReader"
+	to_png(path, name)
+	img = cv2.imread("temp/{}.png".format(name), 0)
+	histr = cv2.calcHist([img], [0], None, [256], [0, 256])
+	plt.plot(histr)
+	plt.show()
 
 
-main()
+if __name__ == "__main__":
+	main()
